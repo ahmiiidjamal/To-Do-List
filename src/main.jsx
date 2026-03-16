2664282b20f53b023f5fe4b8c4421270d7df905e
ahmiiidjamal <ahmedalsoni190@gmail.com>
2026-03-16 19:07:03 +0300

    تعديل routing وإضافة basename

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/To-Do-List">
       <App />
     </BrowserRouter>
   </StrictMode>,
--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base:"/To-Do-List"
+  base:"/To-Do-List/"
 })
